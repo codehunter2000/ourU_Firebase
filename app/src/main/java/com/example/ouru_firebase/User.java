971b9dd400dbfa64eab58abd34d6df0df29721f2
codehunter2000 <gabrielrhunt@gmail.com>
2019-04-12 13:34:43 -0700

    Initial commit.

---
+++ app/src/main/java/com/example/ouru_firebase/User.java
@@ -0,0 +1,42 @@
+package com.example.ouru_firebase;
+
+public class User {
+    private String firstName, lastName, email;
+
+    public User() {
+        firstName = null;
+        lastName = null;
+        email = null;
+    }
+
+    public User(String firstName, String lastName, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
