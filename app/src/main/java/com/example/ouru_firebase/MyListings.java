1b7f8842329066035ab8fa05e5eb170370847759
codehunter2000 <gabrielrhunt@gmail.com>
2019-05-13 11:03:55 -0700

    Added ability for users to delete their listings.

--- app/src/main/java/com/example/ouru_firebase/MyListings.java
+++ app/src/main/java/com/example/ouru_firebase/MyListings.java
@@ -1,7 +1,10 @@
 package com.example.ouru_firebase;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,21 +17,25 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
-public class MyListings extends AppCompatActivity
-{
+public class MyListings extends AppCompatActivity {
 
     private static final String FILE_NAME = "/data/user/0/com.example.ouru_firebase/files/user.dat";
-    private static final String TAG = "AddListing Activity";
+    private static final String TAG = "MyListings";
     private User user = null;
     private ArrayList<Listing> listings;
     private ListView listView;
     private ArrayAdapter arrayAdapter;
-    private DatabaseReference databaseReference;
+    private DatabaseReference databaseReference, listingReferene;
+    private StorageReference storeageRefernce, pictureReference;
+    private Listing tempListing = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,65 +43,87 @@ public class MyListings extends AppCompatActivity
         setContentView(R.layout.activity_my_listings);
 
         user = getUserInfo();
-        listView = (ListView)findViewById(R.id.listview);
+        listView = (ListView) findViewById(R.id.listview);
         databaseReference = FirebaseDatabase.getInstance().getReference("listings");
+        storeageRefernce = FirebaseStorage.getInstance().getReference("images");
+
         listings = new ArrayList<>();
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Edit Listing")
+                .setMessage("Select An Options")
+                .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listingReferene = databaseReference.child(Integer.toString(tempListing.hashCode()));
+                        listingReferene.removeValue(new DatabaseReference.CompletionListener() {
+                            @Override
+                            public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                                Log.v(TAG, "Listing remove successful");
+                            }
+                        });
+                        dialog.dismiss();
+                    }
+                })
+                .setNeutralButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // show listing options
+                tempListing = (Listing) listView.getItemAtPosition(position);
+                builder.show();
             }
         });
 
     }
 
     @Override
-    protected void onStart()
-    {
+    protected void onStart() {
         super.onStart();
-        databaseReference.addValueEventListener(new ValueEventListener()
-        {
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
-            {
-                for (DataSnapshot temp : dataSnapshot.getChildren())
-                {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot temp : dataSnapshot.getChildren()) {
                     Listing listing = temp.getValue(Listing.class);
                     if (listing != null)
                         if (listing.getEmail().equals(user.getEmail()))
                             listings.add(listing);
                 }
                 arrayAdapter = new ArrayAdapter(
-                        MyListings.this,android.R.layout.simple_list_item_1, listings);
+                        MyListings.this, android.R.layout.simple_list_item_1, listings);
                 listView.setAdapter(arrayAdapter);
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError)
-            {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
     }
 
-    private User getUserInfo()
-    {
-        try
-        {
+    private User getUserInfo() {
+        try {
             FileInputStream inputStream = new FileInputStream(FILE_NAME);
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
             User temp = (User) objectInputStream.readObject();
             objectInputStream.close();
             inputStream.close();
             return temp;
-        }
-
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             Log.v(TAG, "Error reading user data");
         }
 
         return null;
     }
+
 }
